2e742bb829ad7fd2a9218cf9ef83156e85367697
Sahil Tiwari <162821589+tiwari02122003@users.noreply.github.com>
2025-04-11 00:14:46 +0530

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,9 @@
+def call(String url,String branch){
+  
+  git url: "${url}", branch: "${branch}"
+  
+
+                //   echo 'Cloning the code from the repository'
+                // git url: "https://github.com/tiwari02122003/django-notes-app.git", branch: "main"
+                // echo "Code Cloning successful"
+}
